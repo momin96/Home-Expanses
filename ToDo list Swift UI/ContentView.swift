ddcc1a011cbb516e325f90ee74bd8827432eb010
Nasir Ahmed Momin <nasir_momin@apple.com>
2020-02-05 15:05:10 +0530

    Initial Commit

---
+++ ToDo list Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo list Swift UI
+//
+//  Created by Nasir Ahmed Momin on 05/02/20.
+//  Copyright © 2020 Nasir Ahmed Momin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
