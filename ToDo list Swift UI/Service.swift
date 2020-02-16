28c314e212f61a2264f0f3b423e83cb651a4af39
Nasir Ahmed Momin <momin96@gmail.com>
2020-02-16 16:33:30 +0530

    Added new service file

---
+++ ToDo list Swift UI/Service.swift
@@ -0,0 +1,93 @@
+//
+//  Service.swift
+//  E Com SwiftUI
+//
+//  Created by Nasir Ahmed Momin on 14/02/20.
+//  Copyright © 2020 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+let KEY_EXPANSES = "Expanses"
+
+struct Service {
+
+    static var shared = Service()
+    
+    private var database: Firestore
+    
+    init() {
+        FirebaseApp.configure()
+        database = Firestore.firestore()
+    }
+    
+    func getAllDocuments() {
+        database.collection(KEY_EXPANSES).getDocuments { (querySnapshot, err) in
+//            print("querySnapshot  \(querySnapshot)")
+//            print("err  \(err)")
+            if let documents = querySnapshot?.documents {
+                for doc in documents {
+                    let documentId = doc.documentID
+                    let data = doc.data()
+                    print(data)
+                    print("-----")
+                    
+                    self.getDocument(documentId)
+                    
+                }
+            }
+        }
+    }
+    
+    func getDocument(_ documentId: String) {
+               database.collection(KEY_EXPANSES).document(documentId).collection("Today").getDocuments { (querySnapshot, err) in
+                   if let documents = querySnapshot?.documents {
+                       for document in documents {
+                           let docId = document.documentID
+                           let str = String(describing: document.data())
+                           let itemPrice = ItemPrice.decodeData(str, documentId: docId)
+                           print(itemPrice as Any)
+                       }
+                   }
+               }
+           }
+    
+}
+
+struct ParentDocument: Codable {
+    var sumOfPrices: Double
+    var noOfEntries: Int
+    
+    var documents: [ItemPrice]
+    
+    
+    
+}
+
+struct ItemPrice: Codable {
+    var item: String
+    var price: Int
+    var documentId: String
+    
+    static func decodeData(_ jsonString: String, documentId: String) -> ItemPrice? {
+        let jsonDecoder = JSONDecoder()
+        
+        if let jsonData = jsonString.data(using: .utf8) {
+            do {
+                let childDoc = try jsonDecoder.decode(ItemPrice.self, from: jsonData)
+                print(childDoc.item)
+                return childDoc
+            }
+            catch (let err){
+                print(err.localizedDescription)
+                return nil
+            }
+        }
+        else {
+            return nil
+        }
+    }
+    
+}
